baa3a857ba249bdc07faeae58934eaf7d7b548bc
AntonJakobsson <anton.jakobsson91@hotmail.com>
2014-02-26 19:00:06 +0100

    logoutbutton

---
+++ src/GUI/LogoutButton.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class LogoutButton extends JButton implements ActionListener
+{
+
+    @Override
+    public void actionPerformed(ActionEvent arg0)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
