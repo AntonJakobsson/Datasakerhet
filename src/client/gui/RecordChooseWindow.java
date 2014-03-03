bcbd23d96b85e472ee52a9a358fb720e2d534bf7
Hchlasmuck <axel_ahlbeck@hotmail.se>
2014-03-03 23:45:14 +0100

    lite shi

--- src/client/gui/RecordChooseWindow.java
+++ src/client/gui/RecordChooseWindow.java
@@ -11,6 +11,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 import common.Record;
 import common.User;
@@ -30,21 +31,21 @@ public class RecordChooseWindow extends JPanel
 	public static final int MESSAGE_NEW = 1;
 	public static final int MESSAGE_CANCEL = 0;
 	private final String[] columnames = { "Record ID", "Doctor", "Nurse", "Division" };
-	private JScrollPane scrollpane;
 
 	public RecordChooseWindow(User user, ArrayList<Record> records)
 	{
 		this.records = records;
 		this.user = user;
-		table = fillMatrix(this.records);
-		scrollpane = new JScrollPane(table);
 		setup();
 	}
 
 	private void setup()
 	{
+		Object[][] data = fillMatrix(this.records);
+		table = new JTable(data, columnames);
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		this.setLayout(new GridLayout(2, 1));
+		JScrollPane scrollpane = new JScrollPane(table);
 		scrollpane.setPreferredSize(new Dimension(10, 80));
 		scrollpane.setColumnHeaderView(new JLabel("HEj"));
 		this.add(new JLabel("Patient: " + user.getName()));
@@ -56,14 +57,12 @@ public class RecordChooseWindow extends JPanel
 	 * @param newRecords updated recordslist
 	 */
 	public void updateWindow(ArrayList<Record> newRecords){
-		reset();
-		table = fillMatrix(newRecords);
-		this.remove(scrollpane);
-		this.add(scrollpane);
-		this.repaint();
+		Object[][] data = fillMatrix(newRecords);
+		DefaultTableModel model = new DefaultTableModel(data, columnames);
+		table.setModel(model);
 	}
 	
-	private JTable fillMatrix(ArrayList<Record> newRecords)
+	private Object[][] fillMatrix(ArrayList<Record> newRecords)
 	{
 		Object[][] data = new Object[newRecords.size()][4];
 		for (int i = 0; i < newRecords.size(); i++)
@@ -74,14 +73,9 @@ public class RecordChooseWindow extends JPanel
 			data[i][2] = record.getNurseName();
 			data[i][3] = record.getDivision();
 		}
-		return new JTable(data, columnames);
+		return data;
 	}
 
-	private void reset(){
-	    DefaultTableModel model = (DefaultTableModel) table.getModel();
-	    model.setRowCount(0);
-	}
-	
 	public Record getSelectedRecord()
 	{
 	    if(table.getSelectedRow() == -1){
